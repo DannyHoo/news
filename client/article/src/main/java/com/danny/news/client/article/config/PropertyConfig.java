cb0966b88ac7872324f4e8712f73a2aaafe67336
dannyhoo <yuyang.hu@opay-inc.com>
2019-11-19 21:06:52 +0800

    update config

---
+++ client/article/src/main/java/com/danny/news/client/article/config/PropertyConfig.java
@@ -0,0 +1,29 @@
+package com.danny.news.client.article.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author huyuyang
+ * @email yuyang.hu@opay-inc.com
+ * @date 2019/11/19下午8:07
+ */
+@Slf4j
+@Component
+public class PropertyConfig {
+    @Value("${project.property.property1}")
+    private String project_property_property1;
+
+    @Value("${common.property.property1}")
+    private String common_property_property1;
+
+    @PostConstruct
+    public void init() {
+        log.info("loadding properties:");
+        log.info("project.property.property1:" + project_property_property1);
+        log.info("common.property.property1:" + common_property_property1);
+    }
+}
