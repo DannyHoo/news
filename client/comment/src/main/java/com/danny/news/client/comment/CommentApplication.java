2baf504b6a0617566d3196f7ef5c5e14c64504e9
dannyhoo <yuyang.hu@opay-inc.com>
2019-11-20 10:11:00 +0800

    article、comment、index、user模块集成hystrix、feign

--- client/comment/src/main/java/com/danny/news/client/comment/CommentApplication.java
+++ client/comment/src/main/java/com/danny/news/client/comment/CommentApplication.java
@@ -2,7 +2,10 @@ package com.danny.news.client.comment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author huyuyang
@@ -11,6 +14,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class CommentApplication {
 
     public static void main(String[] args) {
